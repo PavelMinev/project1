a4cc2406299afd1d13fb25513b73a4d9d79eafe9
PavelMinev <mpl12@rambler.ru>
2020-06-15 20:44:06 +0300

    (feat): three new template engines

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
+// // Handlebars template engine
+// const expressHbs = require('express-handlebars');
 
 const app = express();
 
-const adminRoutes = require('./routes/admin')
+// // Handlebars
+// app.engine('hbs', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs'}))
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminData = require('./routes/admin')
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -14,12 +22,14 @@ app.use('/favicon.ico', (req, res, next) => {
     res.send('No favicon');
 });
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 
 app.use(shopRoutes);
 
 app.use((req, res) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).render('404', {docTitle: 'Page Not Found', path: '/not-found'});
 });
 
 app.listen(3000);
+console.log('App is listening on http://localhost:3000')
--- routes/shop.js
+++ routes/shop.js
@@ -3,11 +3,24 @@ const path = require('path');
 const express = require('express');
 
 const rootDir = require('../util/path');
+const adminData = require('./admin');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+    // console.log(adminData.products);
+    // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+    // template engine on:
+    const products = adminData.products;
+    res.render('shop', { 
+        prods: products, 
+        docTitle: 'Shop', 
+        path: '/', 
+        hasProducts: products.length > 0,
+        activeShop: true,
+        productCSS: true,
+        // layout: false, // Handlebars special keyword - not to use default layout
+    });
 });
 
 module.exports = router;
