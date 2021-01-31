3a420d94ffb6b24e7448137b4b1263f48b0a09d2
PavelMinev <mpl12@rambler.ru>
2021-01-31 17:26:40 +0300

    Start for heroku

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const fs = require('fs');
+const https = require('https');
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -8,11 +10,15 @@ const MongoDBStore = require('connect-mongodb-session')(session);
 const csrf = require('csurf');
 const flash = require('connect-flash');
 const multer = require('multer');
+const helmet = require('helmet');
+const compression = require('compression');
+const morgan = require('morgan');
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
-const MONGODB_URI = 'mongodb+srv://admin:9650276711_M@shop.ub09e.mongodb.net/shop?retryWrites=true&w=majority';
+const MONGODB_URI =
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@shop.ub09e.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}?retryWrites=true&w=majority`;
 
 const app = express();
 const store = new MongoDBStore({
@@ -20,6 +26,9 @@ const store = new MongoDBStore({
     collection: 'sessions'
 });
 const csrfProtection = csrf();
+
+// const privateKey = fs.readFileSync('server.key');
+// const certificate = fs.readFileSync('server.cert');
 
 const fileStorage = multer.diskStorage({
     destination: (req, file, cb) => {
@@ -50,6 +59,15 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
 
+const accessLogStream = fs.createWriteStream(
+    path.join(__dirname, 'access.log'),
+    { flags: 'a' }
+);
+
+app.use(helmet());
+app.use(compression());
+app.use(morgan('combined', {stream: accessLogStream}));
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(multer({
@@ -62,7 +80,7 @@ app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use(
     session({
         secret: '31vj2k154br1dvk413j5hbjv_QE$',
-        resave: false, 
+        resave: false,
         saveUninitialized: false,
         store: store,
         // cookie: {maxAge: 60 * 60 * 24},
@@ -86,16 +104,16 @@ app.use((req, res, next) => {
         return next();
     }
     User.findById(req.session.user._id)
-    .then(user => {
-        if (!user) {
-            return next();
-        }
-        req.user = user;
-        next();
-    })
-    .catch(err => {
-        next(new Error(err));
-    });
+        .then(user => {
+            if (!user) {
+                return next();
+            }
+            req.user = user;
+            next();
+        })
+        .catch(err => {
+            next(new Error(err));
+        });
 })
 
 app.use('/admin', adminRoutes);
@@ -123,7 +141,11 @@ mongoose
     )
     .then(result => {
         console.log('Application is running on http://localhost:3000')
-        app.listen(3000);
+        // https.createServer({
+        //     key: privateKey,
+        //     cert: certificate
+        // }, app).listen(process.env.PORT || 3000);
+        app.listen(process.env.PORT || 3000);
     })
     .catch(err => {
         console.log(err);
